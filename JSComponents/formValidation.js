d3b6271dfe60bfa25c9a891312a1f7d7ef3e1e9b
Max Lichter <iammaxlichter@gmail.com>
2024-04-08 22:42:13 -0500

    saving

---
+++ JSComponents/formValidation.js
@@ -0,0 +1,12 @@
+function validateForm() {
+    var firstName = document.getElementById("firstName").value.trim();
+    var lastName = document.getElementById("lastName").value.trim();
+    var email = document.getElementById("email").value.trim();
+    var message = document.getElementById("message").value.trim();
+
+    if (firstName === "" || lastName === "" || email === "" || message === "") {
+        alert("Please fill in all required fields.");
+        return false; // Prevent form submission
+    }
+    return true; // Allow form submission
+}
---
+++ JSComponents/navbar.js
@@ -0,0 +1,24 @@
+// Function to handle navbar functionality
+function handleNavbar() {
+    // Extract the current page URL parameter
+    var urlParams = new URLSearchParams(window.location.search);
+    var currentPage = urlParams.get('page');
+
+
+    // Get all navbar links
+    var navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+
+    // Loop through each navbar link
+    navLinks.forEach(function(link) {
+        
+        // Check if the link's data-page matches the current page URL parameter
+        if (link.dataset.page === currentPage) {
+            
+            // Add a class to underline the active link
+            link.classList.add('active');
+        }
+    });
+}
+
+// Call the handleNavbar function when the document is fully loaded
+document.addEventListener("DOMContentLoaded", handleNavbar);
