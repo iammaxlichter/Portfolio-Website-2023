16371994173ff2cfcf70fb79e836af7afb0d1e83
Max Lichter <iammaxlichter@gmail.com>
2024-04-29 15:25:10 -0500

    nice

---
+++ JSComponents/typingAnimation.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".auto-type", {
+    strings: ["Software Developer.", "Quality Engineer.", "Web Developer.", "Undergraduate CS Student."],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+})
